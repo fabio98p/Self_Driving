645bec55bcb2f0a89f257199da6335bd1d908004
fabio parisi <parisifabio98@gmail.com>
2026-01-10 14:27:56 +0100

    finish road, and fixed camera

--- road.js
+++ road.js
@@ -10,23 +10,49 @@ class Road{
         const inifnity = 1000000
         this.top = -inifnity
         this.bottom = inifnity
+
+        const topLeft = {x:this.left, y:this.top}
+        const bottomLeft = {x:this.left, y:this.bottom}
+        const topRight = {x:this.right, y:this.top}
+        const bottomRight = {x:this.right, y:this.bottom}
+
+        this.border = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ]
+
+    }
+
+    getLaneCenter(laneIndex){
+        const laneWidth = this.width / this.laneCount
+        return this.left + laneWidth / 2 + Math.min(laneIndex,this.laneCount -1) * laneWidth
     }
 
     draw(ctx){
         ctx.lineWidth = 5
         ctx.strokeStyle = "white"
 
-        for (let i = 0; i <= this.laneCount; i++) {
+        for (let i = 1; i <= this.laneCount -1; i++) {
             const x = leap(
                 this.left,
                 this.right,
                 i/this.laneCount
             )
+
+            ctx.setLineDash([20,20])
             ctx.beginPath()
             ctx.moveTo(x, this.top)
             ctx.lineTo(x, this.bottom)
             ctx.stroke()
         }
 
+        ctx.setLineDash([])
+
+        this.border.forEach(border => {
+            ctx.beginPath()
+            ctx.moveTo(border[0].x, border[0].y)
+            ctx.lineTo(border[1].x, border[1].y)
+            ctx.stroke()
+        })
     }
 }
