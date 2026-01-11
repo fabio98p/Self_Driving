8fa39c88152c6a66e622efadf3687bde9f3e5624
fabio parisi <parisifabio98@gmail.com>
2026-01-11 13:10:00 +0100

    check collision

--- car.js
+++ car.js
@@ -10,38 +10,70 @@ class Car{
         this.maxSpeed = 3
         this.friction = 0.05
         this.angle = 0
+        this.damaged = false
 
         this.sensor = new Sensor(this)
         this.controls = new Controls()
     }
 
     update(roadBorders){
-        this.#move()
+        if (!this.damaged) {   
+            this.#move()
+            this.polygon = this.#createPoligons()
+            this.damaged = this.#assessDamage(roadBorders)
+        }
         this.sensor.update(roadBorders)
     }
 
     draw(ctx){
-        ctx.save()
-        ctx.translate(this.x, this.y)
-        ctx.rotate(-this.angle)
+        console.log(this.damaged);
+        
+        if (this.damaged) {
+            ctx.fillStyle= "grey"
+        }else{
+            ctx.fillStyle= "black"
+        }
 
         ctx.beginPath()
-        ctx.rect(
-            -this.width / 2,
-            -this.height / 2,
-            this.width,
-            this.height
-        )
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
+        for (let i = 1; i < this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+        }
         ctx.fill()
-        ctx.restore()
 
         this.sensor.draw(ctx)
     }
 
+    #assessDamage(roadBorders){
+        for(let i = 0; i < roadBorders.length; i++) {
+            if(polysIntersect(this.polygon, roadBorders[i])){
+                return true
+            }
+        }
+        return false
+    }
+
     #createPoligons(){
         const points = []
-        rad = Math.hypot(this.width, this.height) / 2
+        const rad = Math.hypot(this.width, this.height) / 2
         const alpha = Math.atan2(this.width, this.height)
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad
+        })
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad
+        })
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+        })
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+        })
+        return points
     }
 
     #move(){
